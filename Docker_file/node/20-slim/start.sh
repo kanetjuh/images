59d6b22c111bfd4024ee68d80e428509511e5ca6
kanetjuh <50002439+kanetjuh@users.noreply.github.com>
2025-01-27 17:58:48 +0100

    Update start.sh

--- Docker_file/node/20-slim/start.sh
+++ Docker_file/node/20-slim/start.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -n "$SECOND_CMD" ]; then
-    echo "Executing second startup command: $SECOND_CMD"
-    exec $SECOND_CMD
-elif [ -n "$STARTUP_CMD" ]; then
-    echo "Executing startup command: $STARTUP_CMD"
-    exec $STARTUP_CMD
+# Check if the environment variable $ALVKT(JAR_STARTUP) is set
+if [ -n "$ALVKT(STARTUP)" ]; then
+  # If it's set, run the specified startup command (e.g., running node or another script)
+  echo "Running with custom startup: $ALVKT(STARTUP)"
+  exec $ALVKT(STARTUP)
 else
-    echo "No startup command provided. Starting Node.js REPL..."
-    exec node
+  # If not set, run the default Node.js server
+  echo "Running default Node.js server..."
+  exec node server.js
 fi
